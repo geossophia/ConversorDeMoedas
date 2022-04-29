6344b6332716fbd40e51519dccaccdb2e1c6fd37
geossophia <geossophia@gmail.com>
2022-04-29 09:00:11 -0300

    projeto integrador

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script></script>
+
+<template>
+  <header>
+    <div></div>
+    <h1>Conversor de Moedas</h1>
+    <img src="@/assets/img/denverlacasadepapel.jpg" alt="" />
+    <p>
+      Dolar americano, Euro, Libra Estelina, Lene, Dolar Australiano, Franco
+      Suiço, Dolar Canadense, Renminbi(yuan), Peso argentino, Lira Turca
+    </p>
+    <div></div>
+  </header>
+
+  <main>
+    <div>
+     <input type="number">
+     Valor:
+     <!-- /button// -->
+     <input type="text">
+     Converter De:
+     <!-- /button// -->
+     <p> = </p>
+     <input type="text">
+     Para:
+     <!-- /button// -->
+     <input type="number">
+     Resultado da conversão
+     <!-- /button// -->
+    </div>
+  </main>
+</template>
+
+<style>
+@import "./assets/style.css";
+</style>
