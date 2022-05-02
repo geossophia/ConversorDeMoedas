4e1115d479c23dfdbf4343fea56d6b184c17ec83
Felipe Rotermel <feliperotermell@gmail.com>
2022-05-02 09:20:36 -0300

    alteração no código

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      moedas: [
+        { moeda: "USD", nome: "Dolar americano" },
+        { moeda: "EUR", nome: "Euro" },
+        { moeda: "£", nome: "Libra Esterlina" },
+        { moeda: "JPY", nome: "Lene" },
+        { moeda: "AUD", nome: "Dolar Australiano" },
+        { moeda: "CHF", nome: "Franco Suiço" },
+        { moeda: "CAD", nome: "Dolar Canadense" },
+        { moeda: "CNY", nome: "Renminbi(Yuan)"},
+        { moeda: "ARS", nome: "Peso Argentino"},
+        { moeda: "YTL", nome: "Lira Turca"},
+      ],
+    };
+  },
+};
+</script>
 
 <template>
   <header>
-    <div></div>
-    <h1>Conversor de Moedas</h1>
+    <div class="linhaCima"></div>
     <img src="@/assets/img/denverlacasadepapel.jpg" alt="" />
+    <h1>Conversor de Moedas</h1>
     <p>
       Dolar americano, Euro, Libra Estelina, Lene, Dolar Australiano, Franco
       Suiço, Dolar Canadense, Renminbi(yuan), Peso argentino, Lira Turca
     </p>
-    <div></div>
+    <div class="linhaBaixo"></div>
   </header>
 
   <main>
-    <div>
-     <input type="number">
-     Valor:
-     <!-- /button// -->
-     <input type="text">
-     Converter De:
-     <!-- /button// -->
-     <p> = </p>
-     <input type="text">
-     Para:
-     <!-- /button// -->
-     <input type="number">
-     Resultado da conversão
-     <!-- /button// -->
+    <div class="fundo">
+      <label for="valor">Valor:</label>
+      <input id="valor" placeholder="1,00" type="number" />
+      <label for="conversão">Converter de:</label>
+      <select name="unidades" id="conversão">
+        <option
+          v-for="moeda of moedas"
+          :key="moeda.moeda"
+          for="conversão"
+          value="moeda.moeda"
+        >
+          {{ moeda.nome }}
+        </option>
+      </select>
+      <p>=</p>
+      <input type="text" />
+      Para:
+      <!-- /button// -->
+      <input type="number" />
+      Resultado da conversão
+      <!-- /button// -->
     </div>
   </main>
 </template>
