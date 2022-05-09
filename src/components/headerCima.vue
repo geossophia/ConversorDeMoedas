ec4d0f6e6f7dea5c1e86fa7be4df5564d1c86020
Felipe Rotermel <feliperotermell@gmail.com>
2022-05-09 08:57:38 -0300

    alteração no código

--- src/components/headerCima.vue
+++ src/components/headerCima.vue
@@ -5,15 +5,10 @@ export default {
 </script>
 <template>
     <header>
-        <div class="linhaCima"></div>
         <img src="@/assets/img/lcdp.jpeg" alt="Imagem do personagem Denver de La Casa de Papel" />
         <div id="ts">
             <h1>Conversor de Moedas</h1>
-            <p>
-            Dolar americano, Euro, Libra Estelina, Lene, Dolar Australiano, Franco
-            Suiço, Dolar Canadense, Renminbi(yuan), Peso argentino, Lira Turca
-            </p>
+            <p>Dolar americano, Euro, Libra Estelina, Lene, Dolar Australiano, Franco Suiço, Dolar Canadense, Renminbi(yuan), Peso argentino, Lira Turca</p>
         </div>
-        <div class="linhaBaixo"></div>
-  </header>
+    </header>
 </template>
